b6f6c75c76a57cb0df22dfab0126eedb8a0788c0
Samuel Bruno <leonidaxd1@gmail.com>
2021-11-10 21:08:15 -0300

    Graficos das moedas

--- JAVAlogin.js
+++ JAVAlogin.js
@@ -4,3 +4,32 @@ function openForm() {
 function closeForm() {
     document.getElementById("myForm").style.display = "none";
 }
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+]
+
+const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: "rgb(255, 99, 132)",
+      borderColor: "rgb(255, 99, 13)",
+      data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+}
+
+const config = {
+    type: 'line',
+    data: data,
+    options: {}
+}
+
+const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+)
