2761e524494945d4992385da60c46eafd58ff905
Samuel Bruno <leonidaxd1@gmail.com>
2021-11-29 21:16:33 -0300

    java random 2.0

--- public/JAVAlogin.js
+++ public/JAVAlogin.js
@@ -4,8 +4,8 @@ function randomNumber(min, max) {
 
 let randomDataArray = () => {
     let randomNumbers = [];
-    for(let i = 0; i<7; i++) {
-        randomNumbers.push(randomNumber(0,500));
+    for (let i = 0; i < 7; i++) {
+        randomNumbers.push(randomNumber(0, 500));
     }
     return randomNumbers;
 };
@@ -87,3 +87,25 @@ const myChart3 = new Chart(
     config3
 )
 
+setInterval(() => {
+    for (let i = 1; i < 11; i++) {
+        let num = randomNumber(-10, 10);
+        let elemento = document.getElementById(`${i}-valor`);
+        elemento.textContent = num.toFixed(2);
+        if (num > 0) {
+            elemento.classList.add("positivo");
+            elemento.classList.remove("negativo");
+        } else {
+            elemento.classList.add("negativo");
+            elemento.classList.remove("positivo");
+        }
+    }
+}, 5000)
+
+setInterval(() => {
+    for(let i = 1; i < 6; i++){
+        let num = randomNumber(0,100000);
+        let elemento = document.getElementById(`${i}-preco`);
+        elemento.textContent = num.toFixed(2);
+    }
+}, 5000)
